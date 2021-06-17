e5027c8c64601a5aa8ab505cf30c263412ca3c26
nathayde@hotmail.com <nathayde@hotmail.com>
2021-06-16 22:31:24 -0300

    Products

---
+++ src/main/java/com/cursojava/course/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.cursojava.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cursojava.course.entities.Product;
+import com.cursojava.course.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository Repository;
+	
+	public List<Product> findAll() {
+		return Repository.findAll();
+	}
+
+	public Product findById (Long id) {
+		Optional<Product> obj =  Repository.findById(id);
+		return obj.get();
+	}
+}
